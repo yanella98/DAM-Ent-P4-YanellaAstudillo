456774b12be42a6204fc1a88fa6842bd7c68dbe8
DAM_1 <DAM_1@PB03>
2020-03-06 19:45:24 +0100

    Comprobar lados

--- src/main/java/liceosorolla/Rectangulo.java
+++ src/main/java/liceosorolla/Rectangulo.java
@@ -1,5 +1,42 @@
 package main.java.liceosorolla;
 
 public class Rectangulo {
-
+	
+	private int lado1;
+	private int lado2;
+	
+	
+	public Rectangulo(int lado1, int lado2) {
+		
+		this.lado1 = lado1;
+		this.lado2 = lado2;
+		
+	}
+	
+	public int getLado1() {
+		return lado1;
+	}
+	
+	public void setLado1(int lado1) {
+		this.lado1 = lado1;
+	}
+	
+	public int getLado2() {
+		return lado2;
+	}
+	
+	public void setLado2(int lado2) {
+		this.lado2 = lado2;
+	}
+	
+	public boolean lados() {
+		boolean comprobar = false;
+		
+		if(lado1 == lado2) {
+			comprobar = true;
+			
+		}
+		return comprobar;
+	}
+	
 }
--- src/test/java/liceosorolla/RectanguloTest.java
+++ src/test/java/liceosorolla/RectanguloTest.java
@@ -1,5 +1,22 @@
 package test.java.liceosorolla;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import main.java.liceosorolla.Rectangulo;
+
 public class RectanguloTest {
+	
+	private Rectangulo rectangulo;
+	
+	@Before
+	public void before() {
+		rectangulo = new Rectangulo(12,6);
+	}
+	
+	@Test
+	public void comprobaRectangulo() {
+		assertEquals(false, rectangulo.lados());
+	}
 
 }
