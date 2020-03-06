fda3dcbef72347a4b29a1d7620b8464068a905b5
alexs <alexs@LAPTOP-KQGHEVBO>
2020-03-06 20:42:04 +0100

    numero1

--- src/main/java/liceosorolla/Numero.java
+++ src/main/java/liceosorolla/Numero.java
@@ -1,5 +1,25 @@
 package main.java.liceosorolla;
 
 public class Numero {
+	private int numero1;
+	private int numero2;
+	
+	public int getNumero1() {
+		return numero1;
+	}
+	public void setNumero1(int numero1) {
+		this.numero1 = numero1;
+	}
 
+	public int getNumero2() {
+		return numero2;
+	}
+	public void setNumero2(int numero2) {
+		this.numero2 = numero2;
+	}
+	
+	public Numero(int numero1, int numero2) {
+		this.numero1=numero1;
+		this.numero2=numero2;
+	}
 }
--- src/test/java/liceosorolla/NumeroTest.java
+++ src/test/java/liceosorolla/NumeroTest.java
@@ -1,5 +1,23 @@
 package test.java.liceosorolla;
 
-public class NumeroTest {
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+
+import main.java.liceosorolla.Numero;
+
+public class NumeroTest {
+	
+	private Numero numero;
+	@Before
+	public void Before() {
+		numero=new Numero(5,5);
+	}
+	
+	@Test
+	public void comprobarNumero1() {
+		assertEquals(5,numero.getNumero1());
+	}
 }
