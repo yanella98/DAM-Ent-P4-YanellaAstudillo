1bf4095d666c447f502c42902ed83dadf87b9ad8
yanella98 <yane-@DESKTOP-HOOQ99R.home>
2020-03-20 19:57:36 +0100

    mejora usuario

--- src/main/java/liceosorolla/Usuario.java
+++ src/main/java/liceosorolla/Usuario.java
@@ -1,5 +1,73 @@
 package main.java.liceosorolla;
 
-public class Usuario {
+import java.time.LocalDate;
+import java.time.Period;
 
+public class Usuario {
+	private String nombre;
+	private String apellidos;
+	private LocalDate fechaNacimiento;
+	private int identificador;
+	
+	
+	public Usuario(String nombre, String apellidos, LocalDate fechaNacimiento, int identificador) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.fechaNacimiento = fechaNacimiento;
+		this.identificador = identificador;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getApellidos() {
+		return apellidos;
+	}
+	
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	
+	public int getIdentificador() {
+		return identificador;
+	}
+	
+	public void setIdentificador(int identificador) {
+		this.identificador = identificador;
+	}
+	
+	public String toString() {
+		return "nombre " + nombre + ", apellidos " + apellidos + ", fechaNacimiento " + fechaNacimiento
+				+ ", identificador " + identificador;
+	}
+	
+	public boolean mayoriaEdad() {
+		boolean edad = true;
+		LocalDate ahora = LocalDate.now();
+		
+		Period periodo = Period.between(fechaNacimiento, ahora);
+		edad = 18<periodo.getYears();
+		return edad;
+	}
+	
+	public String nombreMayusculas() {
+		return this.nombre.toUpperCase();
+	}
+	
+	public String nombreMinisculas() {
+		return this.nombre.toLowerCase();
+	}
 }
--- src/test/java/liceosorolla/UsuarioTest.java
+++ src/test/java/liceosorolla/UsuarioTest.java
@@ -1,5 +1,51 @@
 package test.java.liceosorolla;
-
+import static org.junit.Assert.assertEquals;
+import java.time.LocalDate;
+import org.junit.Before;
+import org.junit.Test;
+import main.java.liceosorolla.Usuario;
 public class UsuarioTest {
-
+private Usuario usuario;
+	
+	@Before
+	public void Before() {
+		usuario = new Usuario("Yanella","Astudillo Ortiz", LocalDate.of(2000, 05, 07), 18);
+	}
+	
+	@Test
+	public void comprobarNombre() {
+		assertEquals("Yanella", usuario.getNombre());
+	}
+	
+	@Test
+	public void comprobarApellido() {
+		assertEquals("Astudillo Ortiz", usuario.getApellidos());
+	}
+	
+	@Test
+	public void comprobarNombreCompleto() {
+		assertEquals("Yanella Astudillo Ortiz", usuario.getNombre()+ " "+usuario.getApellidos());
+	}
+	
+	@Test
+	public void comprobarMayorDeEdad() {
+		
+		assertEquals(true,usuario.mayoriaEdad());
+	}
+	
+	@Test
+	public void comprobarIdentificador() {
+		assertEquals(18,usuario.getIdentificador());
+	}
+	
+	@Test
+	public void comprobarNombreMayusculas() {
+		assertEquals("YANELLA",usuario.nombreMayusculas());
+	}
+	
+	@Test
+	public void compruebaNombreMinusculas() {
+		
+		assertEquals("yanella", usuario.nombreMinisculas());
+	}
 }
