5b1ad3f6dabefd06eb34585f08bb1aa89ad32da4
yanella98 <yane-@DESKTOP-HOOQ99R.home>
2020-03-20 21:06:21 +0100

    especie

---
+++ src/main/java/liceosorolla/Animal.java
@@ -0,0 +1,51 @@
+package main.java.liceosorolla;
+
+public class Animal {
+	private String especie;
+	private String raza;
+	private int peso;
+	private int edad;
+	private String colorDePelo;
+	
+	public Animal(String especie, String raza, int peso, int edad, String colorDePelo) {
+		this.especie = especie;
+		this.raza = raza;
+		this.peso = peso;
+		this.edad = edad;
+		this.colorDePelo = colorDePelo;
+	}
+	
+	
+	public String getEspecie() {
+		return especie;
+	}
+	
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+	
+	public String getRaza() {
+		return raza;
+	}
+	
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+	
+	public int getPeso() {
+		return peso;
+	}
+	
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	
+	public int getEdad() {
+		return edad;
+	}
+	
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+}
---
+++ src/test/java/liceosorolla/AnimalTest.java
@@ -0,0 +1,21 @@
+package test.java.liceosorolla;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import main.java.liceosorolla.Animal;
+
+public class AnimalTest {
+	
+	private Animal animal;
+	
+	@Before
+	public void Before() {
+		animal = new Animal("Mamifero","Lince",25,5,"marron");
+	}
+	
+	@Test
+	public void comprobarEspecie() {
+		assertEquals("Mamifero", animal.getEspecie());
+	}
+}
